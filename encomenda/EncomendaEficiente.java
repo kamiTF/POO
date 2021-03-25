b2c94051eb400d0f4e68bcd8295318b05ac8c44a
StrillX <brunofjm@gmail.com>
2021-03-25 18:23:31 +0000

    Encomenda

---
+++ encomenda/EncomendaEficiente.java
@@ -0,0 +1,29 @@
+package encomenda;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+public class EncomendaEficiente {
+    private String nomCliente;
+    private int numFiscal;
+    private String morada;
+    private int numEncomenda;
+    private LocalDateTime data;
+    private ArrayList<LinhaEncomenda> linhas;
+
+    //Diag classes
+    // construtores
+    pbul
+
+
+
+
+    //getters setters
+
+    //equals, toString, clone
+
+    //resolucao do exercicios
+
+    // Entregar :
+    //LinhaEncomenda, EncomendaEficiente, TesteEncomenda e diag de classes num zip
+
+
+}
