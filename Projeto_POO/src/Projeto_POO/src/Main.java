3c2bdceed5eb970bb2bbb06c6596070b938253c2
StrillX <brunofjm@gmail.com>
2021-04-30 11:29:57 +0100

    Class main para executar pequenos testes

---
+++ Projeto_POO/src/Projeto_POO/src/Main.java
@@ -0,0 +1,12 @@
+package Projeto_POO.src;
+
+public class Main {
+    public static void main(String[] args){
+
+        Lateral lat = new Lateral(90,70,80,65,50,75,85,90);
+        Avancado avan = new Avancado();
+        System.out.println("Avaliacao: " + lat.eval());
+        System.out.println("Avaliacao: " + avan.eval());
+
+    }
+}
